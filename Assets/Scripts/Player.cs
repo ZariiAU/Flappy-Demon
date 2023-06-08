8bce2deb6418d49b4856950c5659fbf9736c34ee
Zach <s210504@aie.edu.au>
2023-06-09 00:28:10 +0930

    Added player input, Collision detection with pipes, Scorekeeper

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] private ScoreKeeper scoreKeeper;
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(transform.up * force);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Checkpoint"))
+        {
+            scoreKeeper.Score++;
+            Debug.Log("Score:" + scoreKeeper.Score);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponentInParent<Pipe>())
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    private int score;
+    public int Score { get { return score; } set { score = value; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
